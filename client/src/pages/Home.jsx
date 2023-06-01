160a44abb84cfc4735e44281c7f5008d586a0798
yash patel <yashpatel21102002@gmail.com>
2023-06-01 22:06:58 +0530

    second frontend

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+  return (
+    <Container>
+      Home
+    </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: red;
+
+`
+
+
