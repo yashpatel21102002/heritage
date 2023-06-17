9e2675bbf0205a74f3bad24f0e5be3c98fbabfcb
yash patel <yashpatel21102002@gmail.com>
2023-06-17 14:00:28 +0530

    design check of cards

--- client/src/components/SCartCard.jsx
+++ client/src/components/SCartCard.jsx
@@ -2,11 +2,40 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { IoCalendar } from "react-icons/io5";
 import axios from "axios";
+import {Calendar} from "@natscale/react-calendar"
+
+
 
 
 
 function SCartCard({item}) {
   const [monument,setMonument] = useState({})
+  const [date,setDate] = useState()
+  const [adults,setAdults] = useState(1)
+  const [children,setChildren] = useState(0)
+  const [total,setTotal] = useState(0)
+  
+  const minus = ()=>{
+    if(adults-1 > 0){
+      setAdults(adults-1);
+     
+    }
+    
+  }
+
+  const minusChild = ()=>{
+    if(children  > 0){
+      setChildren(children-1)
+    }
+  }
+
+  const plus = ()=>{
+    setAdults(adults+1)
+  }
+  const plusChild = ()=>{
+    setChildren(children+1)
+  }
+
   useEffect(()=>{
     const getMonument = async ()=>{
       const helloMonument = (await axios.get(`http://localhost:8000/api/monument/${item.monumentId}`)).data;
@@ -17,36 +46,72 @@ function SCartCard({item}) {
     getMonument()
   },[])
  
+ 
   return (
     <Ticket>
       <TicketImg src={monument.img === undefined ? "":monument.img[0]}/>
+      <Container>
+        <TicketName>{monument?.name}</TicketName>
+        <TicketName style={{fontSize:"14px",color:"gray",fontWeight:'550'}}>{monument?.city}</TicketName>
+        <TicketName style={{fontSize:"14px",color:"gray",fontWeight:'550'}}>{monument?.state}</TicketName>
+        <Wrapper1>
+          <Sub>
+            <Span><Input placeholder={!date ? `dd/mm/yyyy`:date} onChange={(e)=>{setDate(e.target.value)}}/></Span>
+            <IoCalendar style={{fontSize:'22px'}}/>
+          </Sub>
+          <Sub1>
+            <Filter>
+              <Option>Indian</Option>
+              <Option>foreigner</Option>
+            </Filter>
+          </Sub1>
+        </Wrapper1>
+        <Wrapper2>
+        <Sub3>
+          <Info>
+            Adults-Price : <b>{monument.price === undefined ? "":monument.price[0].adult_price}</b>
+          </Info>
+          <Info>
+            Child-Price : <b>{monument.price === undefined ? "":monument.price[0].child_price}</b>
+          </Info>
+            
+          </Sub3>
+          <Sub4>
+          <Dec>
+            adults: 
+            <button style={{padding:"0 5px"}} onClick={minus}>-</button>
+            <span>{adults}</span>
+            <button style={{padding:"0 5px"}} onClick={plus}>+</button>
 
-      <TicketName>{monument?.name}</TicketName>
+            </Dec>
+            <Inc>
+              children: 
+            <button style={{padding:"0 5px"}} onClick={minusChild}>-</button>
+            <span>{children}</span>
+            <button style={{padding:"0 5px"}} onClick={plusChild}>+</button>
+
+            </Inc>
+            
+            
+          </Sub4>
+        
+
+        </Wrapper2>
+        <Wrapper3>
+          <h2>Total : 625$</h2>
+          <Button>
+            Save Changes
+          </Button>
+          <Button>
+            Delete
+          </Button>
+
+        </Wrapper3>
+
+      </Container>
+        
       
-      <CalenderLogo/>
-
-      <FilterCity>
-        <FilterStateOption selected disabled>
-          Select Tourist Type
-        </FilterStateOption>
-        <FilterStateOption>Indian</FilterStateOption>
-        <FilterStateOption>Foreigner</FilterStateOption>
-      </FilterCity>
-      {/* <H1>no of travelers</H1> */}
-
-      <TIcketText>Travellers Adult:00 Child:00</TIcketText>
-      <TextAdult>Adults</TextAdult>
-      <TextAdult style={{ marginLeft: "30vw" }}>Children</TextAdult>
-
-      <CounterButton>+</CounterButton>
-      <Counter>56</Counter>
-      <CounterButton style={{ marginLeft: "16.5vw" }}>-</CounterButton>
-
-      <CounterButton style={{ marginLeft: "30vw" }}>+</CounterButton>
-      <Counter style={{ marginLeft: "31.8vw" }}>56</Counter>
-      <CounterButton style={{ marginLeft: "33.8vw" }}>-</CounterButton>
-
-      <TicketTotal>Total:500 rupiya</TicketTotal>
+      
     </Ticket>
   );
 }
@@ -56,35 +121,6 @@ export default SCartCard
 
 
 
-// const SliderTicket = styled.div`
-//   display: flex;
-//   width: 45vw;
-//   height: 60vh;
-//   /* background-color: green; */
-//   align-items: center;
-//   /* justify-content: center; */
-//   /* flex-direction: column; */
-//   position: relative;
-//   flex-wrap: wrap;
-//   overflow: hidden;
-//   overflow: scroll;
-
-//   ::-webkit-scrollbar {
-//     width: 6px;
-//   }
-
-//   /* Track */
-//   ::-webkit-scrollbar-track {
-//     background: #eeebeb;
-//     border-radius: 25px;
-//   }
-//   /* Handle */
-//   ::-webkit-scrollbar-thumb {
-//     background: #dd12dd76;
-//     border-radius: 25px;
-//   }
-//   overflow-x: hidden;
-// `;
 
 const Ticket = styled.div`
   width: 41vw;
@@ -103,85 +139,160 @@ const TicketImg = styled.img`
   width: 12vw;
   height: 100%;
   /* background-color: pink; */
+  flex: 2;
   object-fit: cover;
 `;
 
 const TicketName = styled.h3`
-  margin-left: 10px;
-  /* font-size: 27px; */
-  margin-top: 5px;
-  font-weight: 550;
+  padding-left: 5px;
 `;
 
-const Date = styled.div`
-  width: 125px;
-  height: 3vh;
-  background-color: white;
-  margin-top: 5.5vh;
-  margin-left: 12.7vw;
-  position: absolute;
-  border: 2px solid black;
-`;
-const CalenderLogo = styled(IoCalendar)`
-  width: 2vw;
-  height: 4vh;
-  /* background-color: black; */
-  margin-top: 38px;
-  margin-left: 0.5vw;
-`;
+const Container = styled.div`
+  flex: 5;
+  /* background-color: red; */
 
-const FilterStateOption = styled.option``;
 
-const FilterCity = styled.select`
-  margin-top: 4.5vh;
-  margin-left: 3vw;
-  padding-left: 15px;
-  border-radius: 5px;
-  width: 200px;
-  height: 40px;
-  font-size: medium;
-  color: #beb8b8;
-`;
+`
 
-const TicketTotal = styled.h1`
-  position: absolute;
-  margin-top: 190px;
-  margin-left: 200px;
-  font-size: 20px;
-  font-weight: 500;
-`;
+const Wrapper1 = styled.div`
+  
+  /* background-color: blue; */
+  height: 50px;
+  display: flex;
 
-const Counter = styled.div`
-  height: 30px;
-  width: 30px;
-  position: absolute;
-  margin-top: 150px;
-  margin-left: 14.5vw;
+`
+
+const Sub = styled.div`
+  /* background-color: green; */
+  flex: 1;
   display: flex;
   justify-content: center;
+  gap: 2px;
   align-items: center;
-  /* background-color: black; */
-`;
+  cursor: pointer;
 
-const TextAdult = styled.h1`
-  position: absolute;
-  font-weight: 550;
-  font-size: 22px;
-  margin-top: 113px;
-  margin-left: 12.7vw;
-`;
-const CounterButton = styled.button`
-  position: absolute;
-  height: 30px;
-  width: 30px;
-  margin-top: 150px;
-  margin-left: 12.7vw;
-`;
-const TIcketText = styled.div`
-  width: 60%;
-  height: auto;
-  /* background-color: black; */
-  position: absolute;
-  margin-top: 85px;
-  margin-left: 12.7vw;
-`;
+
+`
+
+const Span = styled.span`
+  width: 10vw;
+  /* height: 4vh; */
+  border: 1px solid black;
+  padding: 5px;
+  background-color: #f6f2f6;
+
+
+`
+const Sub1 = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  gap: 2px;
+  align-items: center;
+ 
+
+
+`
+
+const Filter = styled.select`
+  width: 10vw;
+  /* height: 4vh; */
+  border: 1px solid black;
+  padding: 5px;
+  background-color: #f6f2f6;
+
+
+`
+const Option = styled.option`
+  
+
+
+`
+
+
+const Wrapper2 = styled.div`
+  
+  /* background-color: blue; */
+  height: 70px;
+  display: flex;
+
+`
+
+const Sub3 = styled.div`
+  flex: 1;
+  /* background-color: red; */
+  display: flex;
+  justify-content: center;
+  gap: 6px;
+  align-items: center;
+  flex-direction: column;
+ 
+
+
+`
+const Sub4 = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  /* background-color: blue; */
+  gap: 2px;
+  align-items: center;
+  /* flex-direction: column; */
+  /* background-color: red; */
+  gap: 1vw;
+ 
+
+
+`
+
+const Input = styled.input`
+  
+outline: none;
+border: none;
+font-size: 16px;
+background-color:#f6f2f6 ;
+
+`
+
+const Info = styled.span`
+  
+  padding-left: 5px;
+
+
+`
+
+const Dec = styled.div`
+  display: flex;
+  gap: 5px;
+  
+`
+const Inc = styled.div`
+  display: flex;
+  gap: 5px;
+  
+
+`
+
+const Wrapper3 = styled.div`
+  background-color: #fd95fd;
+  height: 42.5px;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+  /* justify-content: space-between; */
+  /* margin-right: 18px; */
+
+
+`
+
+const Button = styled.button`
+  margin-left: 15px;
+  padding: 5px 20px;
+  background-color: magenta;
+  box-shadow: 0 0 3px 0 black;
+  border: none;
+  cursor: pointer;
+  :hover{
+    box-shadow: 0 0 8px 0;
+  }
+`
